12b7607bcf3b4a10067953c5bd0f130cb42252b5
slim <sjt28003@gmail.com>
2025-05-09 20:26:54 +0200

    kech

---
+++ dom/script.js
@@ -0,0 +1,133 @@
+let hello = ('hello wordl');
+console. log(hello);
+
+const un = 1;
+console.log(un);
+
+var calcul = 1 + 2;
+
+ 
+
+let maViture2 = new Object();
+maViture2
+
+let maVoiture  = 'audi'
+
+
+
+let deux =2;
+let trois = 3;
+let quatre = 4;
+
+if (deux == 2 ) {
+    console.log('deux est à 2');
+    
+}else {console} 
+
+
+
+
+let note = 15;
+if (note >= 16) {
+    noteUSA = "A";
+
+}else if (note >= 14) {
+    noteUSA = "B";
+
+}else if (note >= 10){
+    noteUSA = "D";
+}else {noteUSA = "F";
+
+}
+console.log("note en France : " + note);
+console.log("Note aux USA : " + noteUSA);
+
+switch (noteUSA) {
+    case "A":
+    console.log('Exellent');
+        break;
+    case "B":
+    console.log('BIEN');
+        break;
+    case "C":
+    console.log('assez bien');
+        break;
+    case "D":
+    console.log('PASSABLE');
+        break;
+    case "F":
+    console.log('echec');
+        break;
+    default:
+    console.log('Exellent');
+        break;
+
+}
+document.getElementById('hello');
+console.log(hello);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const bonjour = document.getElementById("bonjour");
+
+const btn = document.getElementById("toggle");
+btn.addEventListener("click", () => { bonjour.classList.toggle("hidden");
+});
+
+
+
+
+    
+
+
+
